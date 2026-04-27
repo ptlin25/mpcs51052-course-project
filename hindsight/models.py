d72aa2dafc13821e51f397d87618ffdc395c947c
Patrick Lin <ptlin@uchicago.edu>
2026-04-27 13:30:43 -0500

    Add dataclasses for data layer

---
+++ hindsight/models.py
@@ -0,0 +1,39 @@
+from dataclasses import dataclass
+from enum import Enum, StrEnum
+
+
+class Position(Enum):
+    GKP = 1
+    DEF = 2
+    MID = 3
+    FWD = 4
+
+
+class Chip(StrEnum):
+    BB = "bboost"
+    TC = "3xc"
+    FH = "freehit"
+    WC = "wildcard"
+
+
+@dataclass
+class Player:
+    id: int
+    name: str
+    position: Position
+    history: list[int]
+
+
+@dataclass
+class Pick:
+    player_id: int
+    multiplier: int
+
+
+@dataclass
+class Gameweek:
+    id: int
+    active_chip: Chip | None
+    points: int
+    points_on_bench: int
+    picks: list[Pick]
