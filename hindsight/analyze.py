f7b0b97a81a2ad18ee921187085c7ba7f49c9da2
Patrick Lin <ptlin@uchicago.edu>
2026-05-18 16:16:58 -0500

    Add docstrings

--- hindsight/analyze.py
+++ hindsight/analyze.py
@@ -1,40 +1,46 @@
-import asyncio
 from dataclasses import dataclass
 from typing import NamedTuple
 
-from hindsight.data import fetch
 from hindsight.models import Position, Chip, Player, Gameweek
 
 
 class PlayerPoints(NamedTuple):
+    """NamedTuple that contains a Player and the points he scored"""
+
     player: Player
     points: int
 
 
 @dataclass(frozen=True)
 class GameweekAnalysis:
-    round: int
+    """Frozen dataclass that contains the analysis of a gameweek"""
+
+    round: int  # gameweek number
     active_chip: Chip | None  # chip played this gameweek
-    transfers_cost: int
-    total_squad_points: int
-    captain: PlayerPoints | None
-    starters: frozenset[PlayerPoints]
-    optimal_starters: frozenset[PlayerPoints]
+    transfers_cost: int  # cost of transfers made this gw
+    total_squad_points: int  # total points scored by the 15-man team
+    captain: PlayerPoints | None  # gw captain (after auto-subs)
+    starters: frozenset[PlayerPoints]  # actual starting 11
+    optimal_starters: frozenset[PlayerPoints]  # optimal starting 11
 
     @property
     def captain_points(self) -> int:
+        """Points scored by the captain"""
         return self.captain.points if self.captain else 0
 
     @property
     def raw_points(self) -> int:
+        """Points scored by the actual starters without bonuses"""
         return sum(p.points for p in self.starters)
 
     @property
     def optimal_raw_points(self) -> int:
+        """Points scored by the optimal starters without bonuses"""
         return sum(p.points for p in self.optimal_starters)
 
     @property
     def should_bench(self) -> list[PlayerPoints]:
+        """Starters that should have been benched"""
         return sorted(
             [p for p in self.starters - self.optimal_starters],
             key=lambda p: p.player.position.value,
@@ -42,6 +48,7 @@ class GameweekAnalysis:
 
     @property
     def should_start(self) -> list[PlayerPoints]:
+        """Substitutes that should have started"""
         return sorted(
             [p for p in self.optimal_starters - self.starters],
             key=lambda p: p.player.position.value,
@@ -49,36 +56,46 @@ class GameweekAnalysis:
 
     @property
     def optimal_captain(self) -> PlayerPoints:
+        """Highest scoring player in the squad"""
         best = max(self.optimal_starters, key=lambda p: p.points)
+        # keep actual captain if the actual captain is a highest scoring player
         if self.captain and best.points == self.captain.points:
             return self.captain
         return best
 
     @property
     def actual_points_no_chips(self) -> int:
+        """Actual points scored without chip bonuses"""
         return self.raw_points + self.captain_points
 
     @property
     def optimal_points_no_chips(self) -> int:
+        """Optimal points scored without chip bonuses"""
         return self.optimal_raw_points + self.optimal_captain.points
 
     @property
     def points_on_actual_bench(self) -> int:
+        """Points left on the actual bench"""
         return self.total_squad_points - self.raw_points
 
     @property
     def points_on_optimal_bench(self) -> int:
+        """Points left on the optimal bench"""
         return self.total_squad_points - self.optimal_raw_points
 
 
 @dataclass(frozen=True)
 class ChipUsage:
+    """Frozen dataclass to store the chip used and the chip bonus points"""
+
     chip: Chip
     chip_bonus: int
 
 
 @dataclass(frozen=True)
 class SeasonAnalysis:
+    """Frozen dataclass to store the analysis of the season"""
+
     total_transfers_cost: int
     actual_points_no_chips: int
     optimal_points_no_chips: int
@@ -89,18 +106,31 @@ class SeasonAnalysis:
 
     @property
     def actual_chip_bonus(self) -> int:
+        """
+        Total chip bonus points from the actual squad selection and
+        actual chip timing
+        """
         return sum(u.chip_bonus for u in self.actual_chip_usage.values())
 
     @property
     def optimal_selection_chip_bonus(self) -> int:
+        """
+        Total chip bonus points with the optimal squad selection and
+        actual chip timing
+        """
         return sum(u.chip_bonus for u in self.optimal_selection_chip_usage.values())
 
     @property
     def optimal_chip_bonus(self) -> int:
+        """
+        Total chip bonus points with the optimal squad selection and
+        optimal chip timing
+        """
         return sum(u.chip_bonus for u in self.optimal_chip_usage.values())
 
     @property
     def actual_total_points(self) -> int:
+        """Total points"""
         return (
             self.actual_points_no_chips
             + self.actual_chip_bonus
@@ -109,32 +139,51 @@ class SeasonAnalysis:
 
     @property
     def optimal_selection_total_points(self) -> int:
+        """Total points after selection optimizations"""
         return self.optimal_points_no_chips + self.optimal_selection_chip_bonus
 
     @property
     def optimal_total_points(self) -> int:
+        """Total points after selection and chip optimizations"""
         return self.optimal_points_no_chips + self.optimal_chip_bonus
 
     def actual_chip_bonus_for(self, round: int) -> int:
+        """Actual chip bonus for the gameweek"""
         usage: ChipUsage | None = self.actual_chip_usage.get(round)
         return usage.chip_bonus if usage else 0
 
     def optimal_selection_chip_bonus_for(self, round: int) -> int:
+        """Chip bonus for the gameweek with optimal selection"""
         usage: ChipUsage | None = self.optimal_selection_chip_usage.get(round)
         return usage.chip_bonus if usage else 0
 
     def optimal_chip_bonus_for(self, round: int) -> int:
+        """Chip bonus for the gameweek with optimal selection and chip timing"""
         usage: ChipUsage | None = self.optimal_chip_usage.get(round)
         return usage.chip_bonus if usage else 0
 
 
 class Analyzer:
-    def __init__(self, team_id: int):
-        self._gameweeks: dict[int, Gameweek] = asyncio.run(fetch(team_id))
+    """Class that takes in raw data and analyzes it"""
+
+    def __init__(self, gameweeks: dict[int, Gameweek]):
+        self._gameweeks = gameweeks
 
     def _get_optimal_starters(self, round: int) -> set[PlayerPoints]:
+        """
+        Returns the optimal starting 11
+
+        This function aggregates the players by position and sorts by
+        the number of points scored. A valid starting 11 needs exactly
+        one goalkeeper, at least 3 defenders, at least 2 midfielders,
+        and at least 1 forward. The minimum requirements are filled
+        and then the remaining 4 players are chosen from the highest-
+        scoring, remaining outfielders (defenders, midfielders, or
+        forwards).
+        """
         gameweek: Gameweek = self._gameweeks[round]
 
+        # get the players by position
         by_pos: dict[Position, list[PlayerPoints]] = {pos: [] for pos in Position}
         for pick in gameweek.picks:
             player: Player = pick.player
@@ -142,6 +191,7 @@ class Analyzer:
             points: int = player.get_gameweek_points(round)
             by_pos[pos].append(PlayerPoints(player, points))
 
+        # sort by points scored descending
         for pos in by_pos:
             by_pos[pos].sort(key=lambda pd: -pd.points)
 
@@ -163,6 +213,7 @@ class Analyzer:
         return set(selected)
 
     def analyze_gameweek(self, round: int) -> GameweekAnalysis:
+        """Returns a GameweekAnalysis object for the given gameweek"""
         gameweek: Gameweek = self._gameweeks[round]
         starters: set[PlayerPoints] = set()
         captain: PlayerPoints | None = None
@@ -191,6 +242,12 @@ class Analyzer:
         )
 
     def _get_actual_chip_timing(self) -> dict[int, Chip]:
+        """
+        Returns the actual chip timing.
+
+        Returns a dict mapping gameweek to the chip played if
+        a chip was actually played that gameweek.
+        """
         chip_timing: dict[int, Chip] = {}
         for round, gameweek in self._gameweeks.items():
             if (chip := gameweek.active_chip) is not None:
@@ -200,6 +257,19 @@ class Analyzer:
     def _get_optimal_assignment(
         self, tc_scores: dict[int, int], bb_scores: dict[int, int]
     ) -> tuple[int, int]:
+        """
+        Returns the optimal Triple Captain and Bench Boost chip timing.
+
+        Given the Triple Captain scores (dict mapping gameweek to
+        chip bonus points if the Triple Captain chip was played that
+        gameweek) and the Bench Boost scores (dict mapping gameweek
+        to chip bonus points if the Bench Boost chip was played that
+        gameweek), returns optimal assignment of chips to maximize
+        the bonus points as a tuple (best Triple Captain gw, best
+        Bench Boost gw).
+
+        Note: only one chip can be played in a gameweek.
+        """
         best_tc_gw: int = max(tc_scores, key=lambda gw: tc_scores[gw])
         best_bb_gw: int = max(bb_scores, key=lambda gw: bb_scores[gw])
 
@@ -228,11 +298,22 @@ class Analyzer:
             ),
         ]
 
+        # return the best assignment
         return max(candidates, key=lambda pair: tc_scores[pair[0]] + bb_scores[pair[1]])
 
     def _get_optimal_chip_timing(
         self, gw_analyses: dict[int, GameweekAnalysis]
     ) -> dict[int, Chip]:
+        """
+        Returns the optimal chip timing.
+
+        Returns a dict mapping gameweek number to the chip used that
+        gameweek in the optimal chip timing. This program does not
+        optimize Free Hit or Wildcard, so the optimal timing for
+        those chips will be the same as the actual timing. Each chip
+        can be played once in gameweeks 1-19 and once in gameweeks
+        20-38. Only one chip can be used during a given gameweek.
+        """
         actual_chip_timing = self._get_actual_chip_timing()
         optimal_chip_timing: dict[int, Chip] = {
             gw: chip
@@ -243,7 +324,16 @@ class Analyzer:
         def _get_optimal_chip_timing_half(
             half_analyses: dict[int, GameweekAnalysis],
         ) -> None:
-            # Score each GW for TC and BB value
+            """
+            Finds the optimal Triple Captain and Bench Boost timing
+            for a half of the season.
+
+            Each gameweek is scored by the points that would be
+            gained if the chip was played that gameweek. For Triple
+            Captain, the chip bonus is the points the optimal
+            captian scored. For Bench Boost, the chip bonus is the
+            points left on the optimal bench.
+            """
             tc_scores: dict[int, int] = {
                 gw: a.optimal_captain.points
                 for gw, a in half_analyses.items()
@@ -271,12 +361,15 @@ class Analyzer:
         return optimal_chip_timing
 
     def analyze_season(self) -> SeasonAnalysis:
+        """Returns a SeasonAnalysis object from the raw gameweek data."""
         gw_analyses: dict[int, GameweekAnalysis] = {
             round: self.analyze_gameweek(round) for round in self._gameweeks
         }
 
         actual_chip_timing: dict[int, Chip] = self._get_actual_chip_timing()
-        optimal_chip_timing: dict[int, Chip] = self._get_optimal_chip_timing(gw_analyses)
+        optimal_chip_timing: dict[int, Chip] = self._get_optimal_chip_timing(
+            gw_analyses
+        )
 
         actual_chip_usage: dict[int, ChipUsage] = {}
         optimal_selection_chip_usage: dict[int, ChipUsage] = {}
